d6f301d26c7738648c01cb4aaabf7d337a3c11a0
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-05-24 14:25:38 +0300

    Add files via upload

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Каким цветом вывести текст?");
+            foreach(Pattern.Colors c in Enum.GetValues(typeof(Pattern.Colors)))
+            {
+                Console.WriteLine($"{(int)c} - {c}");
+            }
+            Console.WriteLine("Введите цифру цвета: ");
+            int color = int.Parse(Console.ReadLine());
+            Pattern.Print(stroka, color);
+            Console.ReadLine();
+        }
+    }
+}
